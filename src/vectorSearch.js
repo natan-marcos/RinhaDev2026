9275c6619a3cddb0d57574fa173befc2b539b137
Felipe M <pinhoes1000@gmail.com>
2026-05-07 12:52:13 -0300

    model / ajustes no vector

--- src/vectorSearch.js
+++ src/vectorSearch.js
@@ -1,7 +1,9 @@
 async function vectorSearch(){
     try {
+        
+
         await new Promise(resolve => setTimeout(resolve, 1000));
-        return "running vector search";
+        
     } catch (error) {
         console.error("Error during vector search:", error);
         throw error;
